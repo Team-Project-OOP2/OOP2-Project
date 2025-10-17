ed3c8d5036a2654b0c03d429c8877b2efdec5116
Altancho04 <altan04@abv.bg>
2025-10-17 15:23:44 +0300

    Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.hotelmanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.hotelmanagementsystem to javafx.fxml;
+    exports org.example.hotelmanagementsystem;
+}
